2bb99ddd5be9a2e4ffc347230d44103c0054af32
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2025-10-16 16:35:50 +0200

    idk

--- UI/Scripts/inv_grid_colums.gd
+++ UI/Scripts/inv_grid_colums.gd
@@ -7,15 +7,11 @@ func _ready() -> void:
 	invgrid.columns = int(size.x/(50*Settings.gui_scale+5))
 	var margin:int = int((size.x - (50*Settings.gui_scale+5)*invgrid.columns)/2)
 	$"MarginContainer".add_theme_constant_override("margin_left", margin)
-	var i:int
 	add_item(ItemsList.Items[0],1)
 	add_item(ItemsList.Items[1],1)
-	add_item(ItemsList.Items[2],1)
-	add_item(ItemsList.Items[3],1)
+	add_item(ItemsList.Items[2],500)
+	add_item(ItemsList.Items[3],0)
 	add_item(ItemsList.Items[4],1)
-	while i < 500 :
-		i=i+1
-		add_item(ItemsList.Items[2],i)
 
 func _on_window_resized():
 	size.y= get_viewport().get_rect().size.y
